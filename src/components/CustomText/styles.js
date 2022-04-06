c82b87667cc8fb2056d199af20317d4effb5dd82
EsioNutrien <Esio.Freitas@nutrien.com>
2022-04-06 07:15:34 -0300

    upgrade custom text component

---
+++ src/components/CustomText/styles.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  headline1: {
+    fontSize: 28,
+    fontWeight: "bold",
+  },
+  headline2: {
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+  subtitle1: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  subtitle2: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  body: {
+    fontSize: 16,
+  },
+  caption: {
+    fontSize: 14,
+  },
+});
