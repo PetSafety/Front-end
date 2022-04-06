2a451cd8e5dfb4de325e9a077da2d96659393226
EsioNutrien <Esio.Freitas@nutrien.com>
2022-04-06 07:15:20 -0300

    upgrade custom button component

---
+++ src/components/CustomButton/styles.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  default: {
+    padding: 11,
+    backgroundColor: "#2689FA",
+    borderRadius: 4,
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 4,
+  },
+  disabled: { backgroundColor: "#656565" },
+  ghost: { backgroundColor: "#fff", borderColor: "#2689FA", borderWidth: 4 },
+  ghostWarning: {
+    backgroundColor: "#fff",
+    borderColor: "#E12E2E",
+    borderWidth: 4,
+  },
+  defaultText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  disabledText: { color: "#fff" },
+  ghostText: { color: "#51A1FB" },
+  ghostWarningText: { color: "#E12E2E" },
+});
